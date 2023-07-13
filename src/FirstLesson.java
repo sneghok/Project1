5a03f0c029ea7be2cc16dcbf7c84991d3fa4cb00
Yulia <jpozova@casgate.io>
2023-07-13 19:51:31 +0300

    Initial commit

---
+++ src/FirstLesson.java
@@ -0,0 +1,13 @@
+public class FirstLesson {
+    int a=5;
+    float f = 23.1f;
+    double d = 24.4;
+    boolean a = true;
+    char ch1 = 'a';
+    byte a = 3;
+    long a = 5;
+    short a = 3;
+    String s;
+
+
+}
